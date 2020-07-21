ecfd265b44d958d9d54c397eb18f4da007968b84
joe sahrmann <joexs10@gmail.com>
2020-07-21 12:49:00 -0500

    start of FieldSurvey project

---
+++ FieldSurvey/FieldSurvey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FieldSurvey
+//
+//  Created by Joe Sahrmann on 7/21/20.
+//  Copyright © 2020 Joe Sahrmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FieldSurvey/FieldSurveyTests/FieldSurveyTests.swift
@@ -0,0 +1,34 @@
+//
+//  FieldSurveyTests.swift
+//  FieldSurveyTests
+//
+//  Created by Joe Sahrmann on 7/21/20.
+//  Copyright © 2020 Joe Sahrmann. All rights reserved.
+//
+
+import XCTest
+@testable import FieldSurvey
+
+class FieldSurveyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
